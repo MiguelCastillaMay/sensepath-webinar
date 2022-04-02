8da526e0de9a747316b159f6919803d053a163c1
Miguel Castilla May <50216221+MiguelCastillaMay@users.noreply.github.com>
2022-04-02 13:01:15 -0600

    Agregué react-router

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Admin from './Components/Users';
+import Comments from './Components/Comments';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="admin" element={<Admin />} />
+      <Route path="admin/usuario/:uid" element={<Comments />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
