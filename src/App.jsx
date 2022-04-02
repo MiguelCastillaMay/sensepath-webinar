d3a9198d2ae740cb16415a0638ad3c348e722d0a
Miguel Castilla May <50216221+MiguelCastillaMay@users.noreply.github.com>
2022-04-02 12:53:59 -0600

    archivo principal

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import "./App.css";
+import firebase from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import SignOut from "./Components/SignOut";
+import SignIn from "./Components/SignIn";
+import ChatRoom from "./Components/ChatRoom";
+import { useRef, useEffect, useState } from "react";
+
+const auth = firebase.auth();
+
+function App() {
+  const [user] = useAuthState(auth);
+  const vidRef = useRef();
+
+  return (
+    <div className="App">
+      <header>
+        <SignOut />
+      </header>
+      <div className="content">
+        <div className="video">
+          <video
+            id="video"
+            width="300"
+            no-controls="true"
+            autoPlay={"autoPlay"}
+            muted
+            ref={vidRef}
+          >
+            <source
+              src="https://firebasestorage.googleapis.com/v0/b/sensepath-chat.appspot.com/o/small.ogv?alt=media&token=0b30d55a-287e-442c-a514-551a841d0c72"
+              type="video/ogg"
+            />
+          </video>
+        </div>
+        <div className="chat">{user ? <ChatRoom /> : <SignIn />}</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
